618a15ad9061610c8a63a7711d3d0ea7f2cef4aa
Malaury <malaurydutour@outlook.fr>
2020-08-18 14:28:21 +0200

    Added a method to import musics from deezer playlist to mongo db

--- Server/Musics/Handler/index.js
+++ Server/Musics/Handler/index.js
@@ -12,11 +12,17 @@ const {
 	SearchMusics,
 } = require('./DeezerHandler');
 const { RefreshEsMusicIndex } = require('../Proxy/ES Proxy');
+const { GetMusicsOfPlaylist } = require('../Proxy/Deezer Proxy/Playlist');
 
 const SearchAndAddMusicsDeezer = async (Query) => {
 	const searchRes = await SearchMusics(Query);
 	await AddMusicsFromDeezer(searchRes);
 	await RefreshEsMusicIndex();
+};
+
+const GetAndAddMusicOfDeezerPlaylist = async (PlaylistDzId) => {
+	const dzMusics = await GetMusicsOfPlaylist(PlaylistDzId);
+	await AddMusicsFromDeezer(dzMusics);
 };
 
 module.exports = {
@@ -27,4 +33,5 @@ module.exports = {
 	IncrementLikeCount,
 	MakeIndexation,
 	SearchAndAddMusicsDeezer,
+	GetAndAddMusicOfDeezerPlaylist,
 };
