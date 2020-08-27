76e85f11af924a73238310adf2c36b7b4f82423e
Malaury <malaurydutour@outlook.fr>
2020-08-27 12:02:01 +0200

    Implement deezer playlist importer on web client

--- Public/Js/Components/Authentification/AccountTopNav.jsx
+++ Public/Js/Components/Authentification/AccountTopNav.jsx
@@ -5,9 +5,8 @@ import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { Button, NavDropdown } from 'react-bootstrap';
 
-import {
-	AddMyAccount as AddMyAccountRedux,
-} from '../../Actions/Action';
+import { AddMyAccount as AddMyAccountRedux } from '../../Actions/Action';
+import PlaylistImporterModal from '../Helper/PlaylistImporterModal';
 
 function mapDispatchToProps(dispatch) {
 	return {
@@ -29,16 +28,16 @@ class AccountTopNavConnected extends React.Component {
 		history: PropTypes.shape({
 			push: PropTypes.func.isRequired,
 		}).isRequired,
-	}
+	};
 
 	static defaultProps = {
 		Account: undefined,
-	}
+	};
 
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			ShowPlaylistImporterModal: false,
 		};
 	}
 
@@ -54,53 +53,76 @@ class AccountTopNavConnected extends React.Component {
 				AddMyAccount(res.data.Account);
 			}
 		});
-	}
+	};
 
 	OnLogout = () => {
 		const { AddMyAccount } = this.props;
 
-		Axios.get('/User/Logout')
-			.then(() => {
-				AddMyAccount(undefined);
-			});
-	}
+		Axios.get('/User/Logout').then(() => {
+			AddMyAccount(undefined);
+		});
+	};
 
 	OnFavorites = () => {
 		const { history } = this.props;
 
 		history.push('/Favorites');
-	}
+	};
 
 	OnHistory = () => {
 		const { history } = this.props;
 
 		history.push('/History');
-	}
+	};
 
 	OnPlaylists = () => {
 		const { history, Account } = this.props;
 
 		history.push(`/User/${Account._id}/Playlists`);
+	};
+
+	OnImportPlaylist = () => {
+		this.setState({ ShowPlaylistImporterModal: true });
+	};
+
+	OnPlaylistModalClose = () => {
+		this.setState({ ShowPlaylistImporterModal: false });
 	}
 
 	render() {
 		const { Account } = this.props;
+		const { ShowPlaylistImporterModal } = this.state;
+
 		if (Account) {
 			const { username } = Account;
 			return (
-				<NavDropdown title={username} id="basic-nav-dropdown" alignRight>
-					<NavDropdown.Item onClick={this.OnFavorites}>Favorites</NavDropdown.Item>
-					<NavDropdown.Item onClick={this.OnHistory}>History</NavDropdown.Item>
-					<NavDropdown.Item onClick={this.OnPlaylists}>Playlists</NavDropdown.Item>
-					<NavDropdown.Divider />
-					<NavDropdown.Item onClick={this.OnLogout}>Logout</NavDropdown.Item>
-				</NavDropdown>
+				<>
+					{ShowPlaylistImporterModal
+						&& <PlaylistImporterModal OnClose={this.OnPlaylistModalClose} />}
+					<NavDropdown title={username} id="basic-nav-dropdown" alignRight>
+						<NavDropdown.Item onClick={this.OnFavorites}>Favorites</NavDropdown.Item>
+						<NavDropdown.Item onClick={this.OnHistory}>History</NavDropdown.Item>
+						<NavDropdown.Item onClick={this.OnPlaylists}>Playlists</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item onClick={this.OnImportPlaylist}>Import Playlist</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item onClick={this.OnLogout}>Logout</NavDropdown.Item>
+					</NavDropdown>
+				</>
 			);
 		}
 		return (
 			<>
-				<Link to="/Register"><Button variant="primary" className="mr-2">Register</Button></Link>
-				<Link to="/Login"><Button variant="outline-primary" className="mr-2">Login</Button></Link>
+				<Link to="/Register">
+					<Button variant="primary" className="mr-2">
+						Register
+					</Button>
+				</Link>
+				<Link to="/Login">
+					<Button variant="outline-primary" className="mr-2">
+						Login
+					</Button>
+				</Link>
 			</>
 		);
 	}
