8e3e5039ab1d3f0479b6f6cd4316c1007e959fa3
Malaury <malaurydutour@outlook.fr>
2021-02-15 18:32:22 +0100

    Added linkered

--- Services/DeezerIndexer/Service/index.ts
+++ Services/DeezerIndexer/Service/index.ts
@@ -8,22 +8,23 @@ import bodyParser from 'body-parser';
 const LogLocation = 'Services.DeezerIndexer';
 const app = express();
 
-ConnectToDB(process.env.MONGO_URL, process.env.USE_MONGO_AUTH === 'true')
-	.then(() => {
-		MopConsole.info(LogLocation, `Connected to mongodb`);
-	})
-	.catch((err) => {
-		MopConsole.error(LogLocation, err);
-		process.exit(1);
-	});
-
 app.use(bodyParser.json());
 app.use(
 	bodyParser.urlencoded({
 		extended: true,
 	})
 );
-app.use('/Album', DeezerAlbumPlugin);
-app.use('/Music', DeezerMusicPlugin);
 
-app.listen(3000, () => MopConsole.info(LogLocation, 'Waiting for requests on 3000'));
+ConnectToDB(process.env.MONGO_URL, process.env.USE_MONGO_AUTH === 'true')
+	.then(() => {
+		MopConsole.info(LogLocation, `Connected to mongodb`);
+		app.get('/ready', (req, res) => res.sendStatus(200));
+		app.use('/Album', DeezerAlbumPlugin);
+		app.use('/Music', DeezerMusicPlugin);
+
+		app.listen(3000, () => MopConsole.info(LogLocation, 'Waiting for requests on 3000'));
+	})
+	.catch((err) => {
+		MopConsole.error(LogLocation, err);
+		process.exit(1);
+	});
